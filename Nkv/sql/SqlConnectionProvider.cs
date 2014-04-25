93d59297fd1b4dbdff1bd6b99c2d61bb53e3cc29
Steven Luu <steven@luu.io>
2014-04-24 21:17:08 -0700

    faggit gitignore did not check in Sql implementation

---
+++ Nkv/sql/SqlConnectionProvider.cs
@@ -0,0 +1,33 @@
+﻿using Nkv.Interfaces;
+using System;
+using System.Data.SqlClient;
+
+namespace Nkv.Sql
+{
+    public class SqlConnectionProvider : IConnectionProvider
+    {
+        private string _connectionString;
+
+        public SqlConnectionProvider(string connectionString)
+        {
+            if (string.IsNullOrWhiteSpace(connectionString))
+            {
+                throw new ArgumentException("SQL Server connection string is required");
+            }
+
+            _connectionString = connectionString;
+
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                this.Database = conn.Database;
+            }
+        }
+
+        public System.Data.IDbConnection GetConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+
+        public string Database { get; private set; }
+    }
+}
---
+++ Nkv/sql/SqlNkv.cs
@@ -0,0 +1,29 @@
+﻿using Nkv.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace Nkv.Sql
+{
+    public class SqlNkv : Nkv
+    {
+        public SqlNkv(SqlConnectionProvider connectionProvider) : base(connectionProvider)
+        {
+        }
+
+        public override void CreateTable<T>()
+        {
+            string tableName = TableAttribute.GetTableName(typeof(T));
+            string query = 
+                @"if not exists (select 1 from sys.tables where name = '{0}')
+                begin
+                    create table [{0}] ([key] nvarchar(128) primary key not null, [value] nvarchar(max), [timestamp] datetime not null)
+                end";
+            query = string.Format(query, tableName);
+            this.ExecuteNonQuery(query);
+        }
+
+    }
+}
