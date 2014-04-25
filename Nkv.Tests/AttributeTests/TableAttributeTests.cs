a44bb1df2acad42bd4c16e5190ef4c66d661d4f4
Steven Luu <steven@luu.io>
2014-04-24 20:01:08 -0700

    Initial commit

---
+++ Nkv.Tests/AttributeTests/TableAttributeTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nkv.Attributes;
+
+namespace Nkv.Tests.AttributeTests
+{
+    [Table()]
+    internal class TypeWithTableAttr { }
+
+    [Table("SomethingElse1")]
+    internal class TypeWithTableAttrAndConstructorName { }
+
+    [Table(Name = "SomethingElse2")]
+    internal class TypeWithTableAttrAndPropName { }
+
+    internal class TypeWithoutAttribute { }
+
+    [TestClass]
+    public class TableAttributeTests
+    {
+        [TestMethod]
+        public void TestGetTableName()
+        {
+            Assert.AreEqual("TypeWithTableAttr", TableAttribute.GetTableName(typeof(TypeWithTableAttr)), true);
+            Assert.AreEqual("SomethingElse1", TableAttribute.GetTableName(typeof(TypeWithTableAttrAndConstructorName)), true);
+            Assert.AreEqual("SomethingElse2", TableAttribute.GetTableName(typeof(TypeWithTableAttrAndPropName)), true);
+            Assert.AreEqual("TypeWithoutAttribute", TableAttribute.GetTableName(typeof(TypeWithoutAttribute)), true);
+        }
+    }
+}
---
+++ Nkv/Attributes/TableAttribute.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Reflection;
+
+namespace Nkv.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class TableAttribute : Attribute
+    {
+        #region Constructors
+
+        public TableAttribute() : this(string.Empty) { }
+
+        public TableAttribute(string name)
+        {
+            this.Name = name;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// The table name used in the database
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Get the table name for a particular object type
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public static string GetTableName(Type type)
+        {
+            var attrs = type.GetCustomAttributes(typeof(TableAttribute), false);
+            if (attrs != null && attrs.Length > 0)
+            {
+                var tableAttr = attrs[0] as TableAttribute;
+                if (tableAttr != null && !string.IsNullOrWhiteSpace(tableAttr.Name))
+                {
+                    return tableAttr.Name;
+                }
+            }
+            
+            return type.Name;
+        }
+    }
+}
